52af4f7ead6c38821410c226eaaa92f2ca368bdf
Khai-Halim <48980700+khai-Halim@users.noreply.github.com>
2020-03-15 11:14:00 +1000

    Stopped questions from changing if nothing is selected

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//DOM Variables
 let submitButton = document.querySelector('.btn>button');
 let answerInputs = document.querySelectorAll('.answer input')
 let questionP = document.querySelector('.question');
@@ -22,79 +23,89 @@ let questions = [
     }
 ];
 
-let questionNumber = 1;
+//Global variables
+let questionNumber = 0;
 let correctAmount;
 let wrongAmount;
+let pos1, pos2, pos3, pos4; //These will randomize 
 
+//Randomize pos variables
+shuffle();
+
+//Initiates first question and answers
 questionP.innerHTML = questions[0].question;
+answerSpans[0].innerHTML = questions[0].answer[pos1];
+answerSpans[1].innerHTML = questions[0].answer[pos2];
+answerSpans[2].innerHTML = questions[0].answer[pos3];
+answerSpans[3].innerHTML = questions[0].answer[pos4];
 
-answerSpans[0].innerHTML = questions[0].answer[0];
-answerSpans[1].innerHTML = questions[0].answer[1];
-answerSpans[2].innerHTML = questions[0].answer[2];
-answerSpans[3].innerHTML = questions[0].answer[3];
+submitButton.addEventListener('click', function(e){
 
-submitButton.addEventListener('click', function(){
+    //TODO check if selected radio button sibling span has the same innerText as the correct answer which I have placed in first position of array.
+    // if(answerInputs[0].checked.nextElementSibling == true){
+    //     console.log('right answer!');
+    //     console.log(e.target);
+    //  } else{
+    //      console.log('wrong answer');
+    //  }
 
-    //check which button has been selected
-    if(answerInputs[0].checked == true){
-        console.log('right answer!');
-     } else{
-         console.log('wrong answer');
-     }
+    //TODO: increase score if correct and increase wrong answer amount if incorrect.
 
-     //TODO: increase score if correct and increase wrong answer amount if incorrect.
-
-     shuffle();
-
-     //initiate new question and answers
-     questionP.innerHTML = questions[questionNumber].question;
-
-     //TODO: change the hard coded numbers in answer[] to be pos variables.
-    answerSpans[0].innerHTML = questions[questionNumber].answer[pos1];
-    answerSpans[1].innerHTML = questions[questionNumber].answer[pos2];
-    answerSpans[2].innerHTML = questions[questionNumber].answer[pos3];
-    answerSpans[3].innerHTML = questions[questionNumber].answer[pos4];
-
-    //uncheck the radio buttons
-    for(let i=0; i<answerInputs.length; i++){
-        answerInputs[i].checked = false;
-    }
-
-    
     //notify that quiz is complete
-    if(questionNumber == 0){
+    if(questionNumber == 3){
         alert('quiz complete');
     }
 
-    //display the next question and answers
-    questionNumber++;
+    //check if answer is correct or wrong
+    console.log("For the question " + questions[questionNumber].question + " The right answer was: " + questions[questionNumber].answer[0]);
 
-    if(questionNumber >= questions.length){
-        questionNumber = 0;
-    }
-    
-    
-    
+    //Alerts the user if no answer has been chosen
+    if(answerInputs[0].checked == false && answerInputs[1].checked == false && answerInputs[2].checked == false && answerInputs[3].checked == false){
+        alert("Please pick an answer");
+    } else{
+        //display the next question and answers
+        questionNumber++;
+
+            //Resets the questions
+        if(questionNumber >= questions.length){
+            questionNumber = 0;
+        }
+
+        console.log(questionNumber);
+
+        //Randomize answer positions
+        shuffle();
+
+        //initiate new question 
+        questionP.innerHTML = questions[questionNumber].question;
+
+        //instantiates new answers
+        answerSpans[0].innerHTML = questions[questionNumber].answer[pos1];
+        answerSpans[1].innerHTML = questions[questionNumber].answer[pos2];
+        answerSpans[2].innerHTML = questions[questionNumber].answer[pos3];
+        answerSpans[3].innerHTML = questions[questionNumber].answer[pos4];
+
+        //uncheck the radio buttons
+        for(let i=0; i<answerInputs.length; i++){
+            answerInputs[i].checked = false;
+        }
+     } 
 });
 
-//Shuffles numbers
-let pos1, pos2, pos3, pos4;
-
+//shuffles the position values.
+//TODO: Shorten this code.
 function shuffle(){
         pos1 = Math.floor(Math.random() * 4);
         pos2 = Math.floor(Math.random() * 4);
         while(pos2 == pos1){
-            console.log(' 2 had to reroll cos it was the same');
             pos2 = Math.floor(Math.random() * 4);
         }
         pos3 = Math.floor(Math.random() * 4);
         while(pos3 == pos1 || pos3 == pos2){
-            console.log(' 3 had to reroll cos it was the same');
             pos3 = Math.floor(Math.random() * 4);
         }
         pos4 = Math.floor(Math.random() * 4);
         while(pos4 == pos1 || pos4 == pos2 || pos4 == pos3){
-            console.log(' 4 had to reroll cos it was the same');
             pos4 = Math.floor(Math.random() * 4);
         }
     
