8d787e481ba0e237b11c746268d6096c18663ec7
Khai <Khai-Halim@users.noreply.github.com>
2020-03-19 14:51:58 +1000

    Added questions about JavaScript string functions to quiz

--- script.js
+++ script.js
@@ -13,24 +13,68 @@ let pos1, pos2, pos3, pos4; //These will randomize
 //Array of questions and answers
 let questions = [
     {
-        question: "What javascript function takes an array and returns a single value?",
-        answer: ["reduce()", "map()", "filter()", "search()"]
+        question: "What string function takes an index number as an argument and returns the character at that index or undefined if no character?",
+        answer: ["charAt()", "search()", "indexOf()", "trim()"]
     },
     {
-        question: "What javascript function returns the index of a string inside a string and can also use regular expressions?",
-        answer: ["search()", "indexOf()", "slice()", "toLowerCase()"]
+        question: "What string function takes a start index and an end index as arguments and returns the part of the string between those index's?",
+        answer: ["slice()", "indexOf()", "charAt()", "every()"]
     },
     {
-        question: "What is a javascript function to turn a string into an array?",
-        answer: ["split()", "filter()", "map()", "some()"]
+        question: "What function takes away the white space around a string?",
+        answer: ["trim()", "filter()", "split()", "length"]
     },
     {
-        question: "What javascript function can return part of a string?",
-        answer: ["slice()", "toUpperCase()", "split()", "indexOf()"]
+        question: "What function takes a number as the argument and repeats the string that many times?",
+        answer: ["repeat()", "replace()", "split()", "some()"]
     },
     {
-        question: "What javascript function can change each item of an array and returns a new array?",
-        answer: ["map()", "slice()", "split()", "reduce()"]
+        question: "What string function takes a string to change and a string to change to as arguments. It returns a new string with the changes?",
+        answer: ["replace()", "slice()", "split()", "reduce()"]
+    },
+    {
+        question: "What string function returns a new string in lowercase?",
+        answer: ["toLowerCase()", "lowerCase()", "toUpperCase()", "changeCase()"]
+    },
+    {
+        question: "What string function returns a new string in uppercase?",
+        answer: ["toUpperCase()", "upperCase()", "charAt()", "every()"]
+    },
+    {
+        question: "What string function takes a string and an index from where to search left to right as arguments and returns the index of that string or returns -1 if not found?",
+        answer: ["indexOf()", "filter()", "split()", "length"]
+    },
+    {
+        question: "What string function takes a string as an argument and returns the index of the last occurance",
+        answer: ["lastIndexOf", "indexOf()", "endIndex()", "toLastIndex()"]
+    },
+    {
+        question: "What string function takes a string or regular expression as an argument and returns the index of that string or returns -1 if not found.",
+        answer: ["search", "find()", "indexOf()", "reduce()"]
+    },
+    {
+        question: "What string function takes a string as an argument and returns all instances of that string in an array?",
+        answer: ["match()", "search()", "indexOf()", "Array.from()"]
+    },
+    {
+        question: "What string function turns cuts a string and returns the sliced parts into an array? The argument decides where to cut",
+        answer: ["split()", "slice()", "splice()", "dice()"]
+    },
+    {
+        question: "What function takes a string as an argument and checks whether the string or array contains that string? It returns true or false",
+        answer: ["includes()", "contains()", "split()", "length"]
+    },
+    {
+        question: "What function takes a string as an argument and checks if the that string exists at the start of a string",
+        answer: ["startsWith()", "beginsWith()", "split()", "some()"]
+    },
+    {
+        question: "What function takes a string as an argument and checks if the that string exists at the end of a string?",
+        answer: ["endsWith()", "lastWord()", "lastIndexOf()", "reduce()"]
+    },
+    {
+        question: "What function counts the amount of index's a string or an array contains and returns that number",
+        answer: ["length", "count()", "endsWith()", "map()"]
     }
 ];
 
